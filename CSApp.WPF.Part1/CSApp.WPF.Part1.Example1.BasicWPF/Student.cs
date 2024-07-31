30b2026dceeb573e94904f410013c001e30b0bf3
corvaroxid <corvaroxid@gmail.com>
2024-07-31 17:08:20 +0300

    Use data binding

---
+++ CSApp.WPF.Part1/CSApp.WPF.Part1.Example1.BasicWPF/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSApp.WPF.Part1.Example1.BasicWPF
+{
+    public class Student
+    {
+        public string StudentName { get; set; }
+        public bool IsEnrolled { get; set; }
+
+        public Student(string name, bool ch)
+        {
+            StudentName = name;
+            IsEnrolled = ch;
+        }
+        public string FullStudentData
+        {
+            get { return StudentName + "\t" + IsEnrolled; }
+        }
+    }
+
+    public class StudentList : ObservableCollection<Student>
+    {
+        public StudentList()
+        {
+            Add(new Student("Jack Brown", true));
+            Add(new Student("John Smith", true));
+            Add(new Student("Alice White", false));
+        }
+    }
+}
